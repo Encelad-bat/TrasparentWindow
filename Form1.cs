1722978db7b7428846a25cddbd8b2c187192a26e
student <student@COMP-202>
2021-02-14 09:45:01 +0200

    GUI  end

--- Form1.cs
+++ Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,6 +13,10 @@ namespace WindowsFormsApp3
 {
     public partial class Form1 : Form
     {
+
+        private Random random;
+        private List<Button> buttons = new List<Button>();
+
         public Form1()
         {
             InitializeComponent();
@@ -19,7 +24,38 @@ namespace WindowsFormsApp3
 
         private void Form1_Load(object sender, System.EventArgs e)
         {
-            
+            this.Size = new Size(600,600);
+            for (int i = 0; i < 2; i++)
+			{
+                Thread.Sleep(11);
+                random = new Random();
+                int x = random.Next(0,this.Size.Width-50);
+                int y = random.Next(0,this.Size.Height-50);
+                Button button = new Button();
+                button.Size = new Size(50,50);
+                button.Location = new Point(x,y);
+                if(buttons.Count == 0)
+                {
+                    if(random.Next(0,1) == 0)
+                    {
+                        button.BackColor = Color.Green;
+                    }
+                    else
+                    {
+                        button.BackColor = Color.Red;
+                    }
+                }
+                else if(buttons[0].BackColor == Color.Green)
+                {
+                    button.BackColor = Color.Red;
+                }
+                else if(buttons[0].BackColor == Color.Red)
+                {
+                    button.BackColor = Color.Green;
+                }
+                buttons.Add(button);
+                this.Controls.Add(button);
+			}
         }
     }
 }
